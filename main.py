dc3f5bea1d60b7c78cfe99e7b8c46cdd9973630d
August Ehrlich <cusd.aquaculture01@gmail.com>
2025-03-03 17:10:35 -0500

    Increases the brightness of the grayscale fish video

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ import numpy as np
 
 
 source = cv2.VideoCapture('data/10_1-Vid2.mp4')
-print(source.get(cv2.CAP_PROP_FPS))
-# We need to set resolutions. 
+
+# We need to set resolutions.
 # so, convert them from float to integer. 
 frame_width = int(source.get(3))
 frame_height = int(source.get(4))
 
 size = (frame_width, frame_height)
 
-result = cv2.VideoWriter('data/output/output.mp4',
+video_name = input()
+result = cv2.VideoWriter('data/output/'+video_name+".mp4",
                          cv2.VideoWriter_fourcc(*'MP4V'),
                          30, size, 0)
+
 
 # running the loop 
 while True:
@@ -23,8 +25,11 @@ while True:
     ret, img = source.read()
     if img is None:
         break
-    # converting to gray-scale 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # converting to gray-scale
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    value = 42  # whatever value you want to add
+    hsv[:, :, 2] = cv2.add(hsv[:, :, 2], value)
+    gray = hsv[:, :, 2]
 
     # write to gray-scale 
     result.write(gray)
