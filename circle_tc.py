60f12a76e66a572ed460fd2509ee5b14001f2678
Rachel Pyeon <rp653@cornell.edu>
2025-03-17 16:55:51 -0400

    Updated test case

---
+++ circle_tc.py
@@ -0,0 +1,46 @@
+import cv2 as cv2
+import numpy as np
+import os
+
+def crop_center_circle(frame):
+    """ Applies cicular mask to frame """
+
+    hh, ww = frame.shape[:2]
+
+    # Center points
+    xc = ww // 2
+    yc = hh // 2
+    
+    radius= yc
+    
+    # Create a mask with a filled white circle
+    mask = np.zeros((hh, ww), dtype=np.uint8)
+    cv2.circle(mask, (xc, yc), radius, 255, thickness=-1)
+
+    # Assuming 3 channels for test case ***
+    MASK = cv2.merge([mask, mask, mask])
+
+    # Apply mask
+    masked_frame = cv2.bitwise_and(frame, MASK)
+
+    # Crop to the square area of the circle
+    cropped_frame = masked_frame[yc - radius:yc + radius, xc - radius:xc + radius]
+
+    return cropped_frame
+
+
+# Load your image
+image_path = '/Users/rachelpyeon/Desktop/fish.jpg'
+frame = cv2.imread(image_path)
+
+cropped = crop_center_circle(frame)
+
+# Create folder
+output_folder = "cropped_frames"
+os.makedirs(output_folder, exist_ok=True)
+
+filename = "cropped_frame_07.jpg"
+cv2.imwrite(os.path.join(output_folder, filename), cropped)
+
+print(f"Saved cropped image to {os.path.join(output_folder, filename)}")
+
