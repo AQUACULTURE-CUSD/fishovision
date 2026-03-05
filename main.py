9af26765c30f49dcdf408423d9ff16159d294cab
Liesel <lyw8@cornell.edu>
2026-03-05 16:45:54 -0500

    fix merge conflict and add comments to main.py

--- main.py
+++ main.py
@@ -1,39 +1,36 @@
 # importing the module
 from __future__ import annotations
 from processing_steps import *
-
 import cv2
 from processing_steps import grayscale
 
 def run_main(pipeline_steps):
+    # load in the pipeline/analysis steps
     cv_pipeline = Pipeline(pipeline_steps)
-
+    # load in the video you want to analyze
     cap = cv2.VideoCapture('data/11_18-Vid11.mov')  # Or 0 for webcam
     if not cap.isOpened():
         print("Error: Could not open video.")
         return
     print(cap.get(cv2.CAP_PROP_FPS))
-
     frame_num = 0
     while True:
         ret, frame = cap.read()
         if not ret:
             break
-
+        # skips frames and reads only 0, 10, 20, etc 
         if (frame_num % 10) != 0:
             frame_num += 1
             continue
-        # 4. Prepare the context for this frame
+        # Prepare the context for this frame
         process_context = {
             'original_frame': frame.copy(),
             'current_frame': frame.copy(),
             'frame_number': frame_num
         }
-
-        # 5. Run the pipeline
+        # Run the pipeline
         context = cv_pipeline.run(process_context)
         frame_num += 1
-
     cap.release()
     cv2.destroyAllWindows()
 
@@ -48,11 +45,13 @@ def test_one_image(pipeline_steps):
             'current_frame': frame.copy(),
             'frame_number': 0
         }
-
         # 5. Run the pipeline
         context = cv_pipeline.run(process_context)
 
 
+'''
+ pipeline steps, associated with files. 
+ Refer to comments or https://docs.google.com/document/d/1XnaYBOWkOAfA_JPGirxE1JfnxZRbWAsDbrj2eBJtMsM/edit?usp=sharing '''
 if __name__ == "__main__":
     pipeline_steps = [
         # MedianFilter(),
